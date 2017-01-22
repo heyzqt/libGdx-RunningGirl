406fe2fcda7dc02ca4e6c7a8331a1ca5e3f3b661
heyzqt <729224643@qq.com>
2017-01-22 11:12:22 +0800

    添加一些参数和public方法

--- core/src/com/heyzqt/girl/MainGame.java
+++ core/src/com/heyzqt/girl/MainGame.java
@@ -3,31 +3,54 @@ package com.heyzqt.girl;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 
 public class MainGame extends Game {
-	SpriteBatch batch;
-	Texture img;
 
-	Box2DDebugRenderer b;
-	
+	//画笔
+	private SpriteBatch batch;
+
+	//精灵相机
+	private OrthographicCamera mCamera;
+
+	//UI相机
+	private OrthographicCamera mUICamera;
+
+	public static String TAG = "mygame";
+
+	// 视距宽度
+	public static final int ViewPort_WIDTH = 320;
+
+	// 视距高度
+	public static final int ViewPort_HEIGHT = 240;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 
-		b = new Box2DDebugRenderer();
+		mCamera = new OrthographicCamera();
+		mCamera.setToOrtho(false, ViewPort_WIDTH, ViewPort_WIDTH);
 
+		mUICamera = new OrthographicCamera();
+		mUICamera.setToOrtho(false,ViewPort_WIDTH,ViewPort_HEIGHT);
+	}
+
+	public SpriteBatch getSpriteBatch(){
+		return batch;
+	}
+
+	public OrthographicCamera getCamera(){
+		return mCamera;
+	}
+
+	public OrthographicCamera getUICamera(){
+		return  mUICamera;
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
 	}
 }
