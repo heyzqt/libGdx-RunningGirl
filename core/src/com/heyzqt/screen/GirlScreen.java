ee6e8c9a2bfd4821831107448b2d354e89fceee3
heyzqt <729224643@qq.com>
2017-01-25 09:56:59 +0800

    更新GirlScreen.java 添加基本参数和update(float delta)抽象方法

--- core/src/com/heyzqt/screen/GirlScreen.java
+++ core/src/com/heyzqt/screen/GirlScreen.java
@@ -1,7 +1,62 @@
 package com.heyzqt.screen;
 
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.heyzqt.girl.MainGame;
+
 /**
  * Created by heyzqt on 2017/1/21.
+ *
+ * 建立抽象的GirlScreen父类
  */
-public class GirlScreen {
+public abstract class GirlScreen implements Screen {
+
+	private OrthographicCamera mCamera;
+	private OrthographicCamera mUICamera;
+	private SpriteBatch mBatch;
+	private MainGame mGame;
+
+	public GirlScreen(MainGame game){
+		this.mGame = game;
+	}
+
+	@Override
+	public void show() {
+		mBatch = mGame.getSpriteBatch();
+		mCamera = mGame.getCamera();
+		mUICamera = mGame.getUICamera();
+	}
+
+	public abstract void update(float delta);
+
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
 }
